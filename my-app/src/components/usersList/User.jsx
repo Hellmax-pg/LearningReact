0c7a498e4b0c770e229eb15da52e1195466caa8b
hellmax <pegashev95@gmail.com>
2021-02-07 21:25:20 +0200

    1

--- my-app/src/components/usersList/User.jsx
+++ my-app/src/components/usersList/User.jsx
@@ -1,9 +1,13 @@
 import './User.css';
 
 function User() {
-  return (
-    <div><b>User</b></div>
-  );
+    return (
+        <div>
+            <b>User 1</b>
+            <b>User 2</b>
+            <b>User 3</b>
+        </div>
+    );
 }
 
 export default User;
